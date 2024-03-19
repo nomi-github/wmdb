445c31fed9bd321a9cd3e3dccc704eed811ede2d
Ariunkhishigz <ariunkhishig.zuzaan@gmail.com>
2024-03-19 16:34:51 -0500

    changed port

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get("/getLocation", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 81;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
