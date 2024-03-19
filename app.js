080da7f2587b7f3108810467733ddbf3b9922398
Ariunkhishigz <ariunkhishig.zuzaan@gmail.com>
2024-03-19 14:47:25 -0500

    backend

--- app.js
+++ app.js
@@ -2,38 +2,31 @@ const express = require("express");
 const axios = require("axios");
 const ejs = require("ejs");
 const path = require("path");
-var cookieParser = require('cookie-parser');
+var cookieParser = require("cookie-parser");
 const { db } = require("./routers/mongodb");
+const router = require("./routers/router");
 const app = express();
 
-
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "html");
 app.engine("html", ejs.renderFile);
 
-
 app.use(express.static("public"));
 app.use("/css", express.static(path.join(__dirname, "views", "css")));
 app.use("/images", express.static(path.join(__dirname, "views", "images")));
 
-app.get('/', async (req, res) => {
-
-  res.render("pages/index");
-});
-
-
-app.get('/getLocation', async (req, res) => {
+app.get("/getLocation", async (req, res) => {
   try {
-      const response = await axios.get('https://ipinfo.io/json');
-      const { loc, city, country, postal } = response.data;
-      const [latitude, longitude] = loc.split(',');
-      res.json({ latitude, longitude, city, country, postal });
+    const response = await axios.get("https://ipinfo.io/json");
+    const { loc, city, country, postal } = response.data;
+    const [latitude, longitude] = loc.split(",");
+    res.json({ latitude, longitude, city, country, postal });
   } catch (error) {
-      console.error('Error fetching user location:', error.message);
-      res.status(500).json({ error: 'Error fetching user location' });
+    console.error("Error fetching user location:", error.message);
+    res.status(500).json({ error: "Error fetching user location" });
   }
 });
 
@@ -41,3 +34,5 @@ const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(router);
