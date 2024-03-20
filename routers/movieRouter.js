47f732c757818577b11f8feb3db1495b8e8b673d
Eman22S <emanawel2@gmail.com>
2024-03-20 00:12:18 -0500

    cleaned up movierouter and added recommender code

--- routers/movieRouter.js
+++ routers/movieRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fetch = require('node-fetch');
 const { getJson } = require("serpapi");
 var cookieParser = require("cookie-parser");
-
+const axios = require("axios");
 const homeHelper = require("./homeHelper");
 const options = {
   caseSensitive: true,
@@ -12,16 +12,9 @@ const router = express.Router(options);
 
 
 router.get('/movie_details/:id', async function(req, res, next){
-  console.log("Hello world")
-  // get the movie details from request object 
   let movie_id = req.params.id;
-  
-
-  //let ulocation = decodeURIComponent(req.params.location)
-  //console.log(typeof ulocation)
   //let movie_id = 636706; this is in theaters
   //let movie_id = 19995; this is not in theaters
-
   let movieurl = 'https://api.themoviedb.org/3/movie/'+movie_id+'?language=en-US';
   const now_playing = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
   let videourl = 'https://api.themoviedb.org/3/movie/'+movie_id+'/videos?language=en-US';
@@ -37,25 +30,14 @@ router.get('/movie_details/:id', async function(req, res, next){
   let prom1 = fetch(movieurl, options)
   let movie = prom1.then(res => res.json())
   let nowplays = fetch(now_playing,options).then(res => res.json())
-  /*let theaters = getJson({
-    q: "Theaters near me",
-    location: ulocation,
-    engine: "google_local",
-    hl: "en",
-    gl: "us",
-    api_key: "bd8997a5a5ad41c2f2f21767535dc7ac1d31fc5e6da73dc7397cc339af51c75b"
-  }, (json) => {
-    //console.log(json['local_results']);
-    return json['local_results']
-  });*/
-
-
   let video = fetch(videourl, options).then((res) => res.json())
-   /*     .then(json => {
-            key = json['results'][0]['key']
-            trailer = "https://www.youtube.com/embed/"+key
-        })*/
-
+  
+  const recommendRes = await axios.get(`https://api.themoviedb.org/3/movie/${movie_id}/recommendations?language=en-US&api_key=${process.env.API_KEY}&page=1`);
+  if (recommendRes.status != 200) {
+    res.render("error");
+  }
+  console.log("rec", recommendRes.data.results[0]);
+  
   Promise.all([movie, nowplays, video]).then(values => {
     let movie = values[0]
     let playimg = "https://image.tmdb.org/t/p/w500"+movie['poster_path']
@@ -65,24 +47,9 @@ router.get('/movie_details/:id', async function(req, res, next){
     let vote_avg = movie['vote_average']
     let vote_count = movie['vote_count']
     let homepg = movie['homepage']
-    /**
-     * fetch on videourl does not give any results for Now Playing
-     * 
-     * 
-     * 
-     */
-    
-    //console.log("Video", values[3])
     let trailer = "https://www.youtube.com/embed/"+values[2]['results'][0]['key']
-    console.log(trailer)
-    //console.log('Now Playing', values[1]['results'])
-
-
-    //console.log('Movies', values[0]['homepage'])
-    //console.log('theaters', values[2]['local_results'])
+    //console.log(trailer)
     let isNow = false
-    //console.log(values[1]['results'])
-
     for (let ele of values[1]['results']){
         if (ele['id'].toString() === movie_id.toString()){
           console.log("Movie is Playing")
@@ -91,19 +58,11 @@ router.get('/movie_details/:id', async function(req, res, next){
       
     }
 
-    /**
-     * 
-     * My google places search for theaters are 
-     * title to know the name
-     * address
-     * thumbnail
-     * description
-     * ratings
-     * 
-    */
-
-    console.log("This is isNow "+isNow)
-  res.render('pages/movie', {data: {t: title, o:overview, v1:vote_avg, v2:vote_count, img:playimg, main:mainimg, mid:movie_id, hg:homepg, /*location:ulocation,*/ isPlaying:isNow, trail:trailer /*theaters: values[2]['local_results']*/ }})
+  res.render('pages/movie', {data: {t: title, o:overview, 
+                                    v1:vote_avg, v2:vote_count, 
+                                    img:playimg, main:mainimg, 
+                                    mid:movie_id, hg:homepg,isPlaying:isNow,
+                                    trail:trailer  }})
   })
   .catch(err => console.error('error:' + err));
 
