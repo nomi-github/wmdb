aa72488caf57b480f863fc3b335ee4191242d025
Nwai Thingyan <nwaithingyan485@gmail.com>
2024-03-20 14:48:48 -0500

    check user already existed

--- routers/credential.js
+++ routers/credential.js
@@ -17,53 +17,54 @@ credentialRouter.post("/signup", async (req, res) => {
     res.render("pages/registerCustom", { msg: "Password length must be between 6 to 30." });
   } else {
     // const user = "";
-    // // const user = db.findUserByUsername(req.body.name);
+    const user = db.findUserByUsername(req.body.name);
     // user = await db.findUserByUsername(req.body.name);
-    // if(!user){
-    const result = db.createUser(req.body.email, req.body.name, req.body.password);
-    console.log(result);
-    if ((await result).acknowledged) {
-      res.cookie("username", req.body.name);
+    if(!user){
+        const result = db.createUser(req.body.email, req.body.name, req.body.password);
+        console.log(result);
+        if ((await result).acknowledged) {
+          res.cookie("username", req.body.name);
 
-      res.cookie("latestMovie", "157336");
-      // res.render("pages/loader");
-      const nowPlayingRes = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&api_key=${process.env.API_KEY}&page=1`);
-      if (nowPlayingRes.status != 200) {
-        res.render("error");
-      }
-      const popularRes = await axios.get(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=${process.env.API_KEY}&page=1`);
-      if (popularRes.status != 200) {
-        res.render("error");
-      }
+          res.cookie("latestMovie", "157336");
+          // res.render("pages/loader");
+          const nowPlayingRes = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&api_key=${process.env.API_KEY}&page=1`);
+          if (nowPlayingRes.status != 200) {
+            res.render("error");
+          }
+          const popularRes = await axios.get(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=${process.env.API_KEY}&page=1`);
+          if (popularRes.status != 200) {
+            res.render("error");
+          }
 
-      let movie_id = req.cookies.latestMovie || 157336;
-      console.log("movie id", movie_id);
-      const recommendRes = await axios.get(`https://api.themoviedb.org/3/movie/${movie_id}/recommendations?language=en-US&api_key=${process.env.API_KEY}&page=1`);
-      if (recommendRes.status != 200) {
-        res.render("error");
-      }
-      console.log("rec", recommendRes.data.results);
-      const upcoming = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?language=en-US&api_key=${process.env.API_KEY}&page=1`);
-      if (upcoming.status != 200) {
-        res.render("error");
-      }
+          let movie_id = req.cookies.latestMovie || 157336;
+          console.log("movie id", movie_id);
+          const recommendRes = await axios.get(`https://api.themoviedb.org/3/movie/${movie_id}/recommendations?language=en-US&api_key=${process.env.API_KEY}&page=1`);
+          if (recommendRes.status != 200) {
+            res.render("error");
+          }
+          console.log("rec", recommendRes.data.results);
+          const upcoming = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?language=en-US&api_key=${process.env.API_KEY}&page=1`);
+          if (upcoming.status != 200) {
+            res.render("error");
+          }
 
-      const genreRes = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}`);
-      if (genreRes.status != 200) {
-        res.render("error");
-      }
-      console.log(req.cookies.isLogged);
-      await res.render("pages/index", {
-        popular: popularRes.data.results,
-        nowPlaying: nowPlayingRes.data.results,
-        recommend: recommendRes.data.results,
-        isLogged: req.cookies.isLogged,
-        genres: genreRes.data.genres,
-      });
+          const genreRes = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}`);
+          if (genreRes.status != 200) {
+            res.render("error");
+          }
+          console.log(req.cookies.isLogged);
+          await res.render("pages/index", {
+            popular: popularRes.data.results,
+            nowPlaying: nowPlayingRes.data.results,
+            recommend: recommendRes.data.results,
+            isLogged: req.cookies.isLogged,
+            genres: genreRes.data.genres,
+          });
+        }
+    }else{
+        // res.render("pages/signup", { msg: 'User already existed.' });
+        res.render("pages/registerCustom", { msg: 'User already existed.' });
     }
-    // }else{
-    //     res.render("pages/signup", { msg: 'User already existed.' });
-    // }
   }
 });
 
